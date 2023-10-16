dfcc9e52cc668c36a1015e0e2c1fbdea7d14b908
ruhuseyn <huseyinovrufet49@gmail.com>
2023-10-16 17:30:16 +0400

    initial

---
+++ src/main/java/az/kiraye/core/Application.java
@@ -0,0 +1,11 @@
+package az.kiraye.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+
+
+}
